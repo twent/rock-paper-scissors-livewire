2f2ecd8e5b56a722db7b9ef372d47f299961f4f0
twent <i@twent.ru>
2023-03-08 00:26:35 +0300

    ✅ Livewire installed

---
+++ app/Http/Livewire/GameField.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class GameField extends Component
+{
+    public function render()
+    {
+        return view('livewire.game-field');
+    }
+}
---
+++ resources/views/livewire/game-field.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Rock Paper Scissors Game</h1>
+</div>
